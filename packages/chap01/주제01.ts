dfcb7145daf8b48022eaf5288169494a12c95069
Blue <blue.cho@hyper-cloud.kr>
2024-03-14 16:47:31 +0900

    FEAT: prettier 적용

--- packages/chap01/주제01.ts
+++ packages/chap01/주제01.ts
@@ -1,36 +1,40 @@
 type Painter = (indicator: number) => void;
 
 const drawLinearProgressDraw: Painter = (indicator) => {
-    // 선형
-}
+  // 선형
+};
 
 const drawCircularProgressDraw: Painter = (indicator) => {
-    // 원형
-}
+  // 원형
+};
 
-const startWork = (painterCallback: Painter, initIndicator = 0, config: {
-                       intervalSecond?: number
-                       completeIndicator?: number
-                       completeCallback?: () => void
-                   } = {
-                       intervalSecond: 1000,
-                       completeIndicator: 100,
-                   }
+type Config = {
+  intervalSecond?: number;
+  completeIndicator?: number;
+  completeCallback?: () => void;
+};
+
+const startWork = (
+  painterCallback: Painter,
+  initIndicator = 0,
+  config: Config = {
+    intervalSecond: 1000,
+    completeIndicator: 100,
+  },
 ) => {
-    let indicator = initIndicator
-    const intervalId = setInterval(() => {
-        if (indicator === config.completeIndicator) {
-            clearInterval(intervalId);
-            config.completeCallback?.();
-        }
-        indicator += 10
-        painterCallback(indicator);
-    }, config.completeIndicator);
-}
-
+  let indicator = initIndicator;
+  const intervalId = setInterval(() => {
+    if (indicator === config.completeIndicator) {
+      clearInterval(intervalId);
+      config.completeCallback?.();
+    }
+    indicator += 10;
+    painterCallback(indicator);
+  }, config.completeIndicator);
+};
 
 startWork(drawLinearProgressDraw, 0, {
-    completeCallback: () => {
-        console.log('작업 완료');
-    }
-})
+  completeCallback: () => {
+    console.log("작업 완료");
+  },
+});
