50de92636d3f7bd8cffca919731ad715889531ed
Blue <blue.cho@hyper-cloud.kr>
2024-03-14 14:03:37 +0900

    FEAT: 함수형으로 변경

---
+++ packages/chap01/주제01.ts
@@ -0,0 +1,36 @@
+type Painter = (indicator: number) => void;
+
+const drawLinearProgressDraw: Painter = (indicator) => {
+    // 선형
+}
+
+const drawCircularProgressDraw: Painter = (indicator) => {
+    // 원형
+}
+
+const startWork = (painterCallback: Painter, initIndicator = 0, config: {
+                       intervalSecond?: number
+                       completeIndicator?: number
+                       completeCallback?: () => void
+                   } = {
+                       intervalSecond: 1000,
+                       completeIndicator: 100,
+                   }
+) => {
+    let indicator = initIndicator
+    const intervalId = setInterval(() => {
+        if (indicator === config.completeIndicator) {
+            clearInterval(intervalId);
+            config.completeCallback?.();
+        }
+        indicator += 10
+        painterCallback(indicator);
+    }, config.completeIndicator);
+}
+
+
+startWork(drawLinearProgressDraw, 0, {
+    completeCallback: () => {
+        console.log('작업 완료');
+    }
+})
